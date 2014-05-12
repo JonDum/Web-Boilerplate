ec65abd7da4d9b85a133bdca0a3efa0b716fbd12
Jonathan Dumaine <jonathan.dumaine@gmail.com>
2014-05-12 09:19:42 -0700

    Add anon wrap function in main.js for better compression

--- js/main.js
+++ js/main.js
@@ -4,75 +4,78 @@
 // @include vendor/TweenMax.js
 // @include vendor/retina.js
 
-// @include utils.js
 // @include polyfills.js
 
-
-/**
- *
- *  Check the page had loaded and is ready to run javascript stuffs
- *
- */
-if(document.readyState == 'complete' || document.readyState == 'interactive')
-    init();
-else
-    document.addEventListener("DOMContentLoaded", init);
+(function(window, document) {
 
 
-function init(window, document)
-{
-    FastClick.attach(document.body);
-
-    if(window.addEventListener)
-        window.addEventListener('hashchange', onHashChange);
+    /**
+     *
+     *  Check the page had loaded and is ready to run javascript stuffs
+     *
+     */
+    if(document.readyState == 'complete' || document.readyState == 'interactive')
+        init();
     else
-        window.onhashchange = onHashChange;
+        document.addEventListener("DOMContentLoaded", init);
 
-    onHashChange();
-}
+    // @include utils.js
 
-
-var hashTargetScrollPosition;
-
-function onHashChange(e)
-{
-    if(!location.hash.length)
-        return;
-
-    var id = location.hash.slice(0, location.hash.length - 1)
-    var hrefTarget = q(id);
-
-    if(hrefTarget)
+    function init()
     {
-        hashTargetScrollPosition = offset(hrefTarget).top - 50;
+        FastClick.attach(document.body);
 
-        TweenLite.to(siteWrap, 0.4, {scrollTo: hashTargetScrollPosition, onComplete: function() {
-            addEventListener('scroll', onScroll)
-        } })
-    }
-}
-
-function onScroll(e)
-{
-    var delta = hashTargetScrollPosition - scrollY;
-    if(Math.abs(delta) > 50)
-    {
-        if('pushState' in history)
-            history.pushState("", document.title, location.pathname + location.search); //Clear the hash
+        if(window.addEventListener)
+            window.addEventListener('hashchange', onHashChange);
         else
-            location.hash.replace(/#.+?/, '');
+            window.onhashchange = onHashChange;
 
-        removeEventListener('scroll', onScroll);
+        onHashChange();
     }
 
-}
+
+    var hashTargetScrollPosition;
+
+    function onHashChange(e)
+    {
+        if(!location.hash.length)
+            return;
+
+        var id = location.hash.slice(0, location.hash.length - 1)
+        var hrefTarget = q(id);
+
+        if(hrefTarget)
+        {
+            hashTargetScrollPosition = offset(hrefTarget).top - 50;
+
+            TweenLite.to(siteWrap, 0.4, {scrollTo: hashTargetScrollPosition, onComplete: function() {
+                addEventListener('scroll', onScroll)
+            } })
+        }
+    }
+
+    function onScroll(e)
+    {
+        var delta = hashTargetScrollPosition - scrollY;
+        if(Math.abs(delta) > 50)
+        {
+            if('pushState' in history)
+                history.pushState("", document.title, location.pathname + location.search); //Clear the hash
+            else
+                location.hash.replace(/#.+?/, '');
+
+            removeEventListener('scroll', onScroll);
+        }
+
+    }
 
 
-function injectScript(src)
-{
-    var s = document.createElement('script');
-    s.src = src;
-    s.async = true;
-    document.body.appendChild(s);
-}
+    function injectScript(src)
+    {
+        var s = document.createElement('script');
+        s.src = src;
+        s.async = true;
+        document.body.appendChild(s);
+    }
 
+})(window, document);
