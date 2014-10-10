946ead0f680788e4ab61cb16250d6d9124ab0c58
Jonathan Dumaine <jonathan.dumaine@gmail.com>
2014-10-10 02:17:28 -0700

    Add plugins directly to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,7 @@ module.exports = {
         }
     },
     plugins: [
-        new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
-    )
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
     ]
 }
