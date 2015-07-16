357778cde25984652c388797bd05be66bc0ac192
Jonathan Dumaine <jonathan.dumaine@gmail.com>
2015-07-16 13:36:00 +0300

    Fix bug in anyparentHasClass
    Missing an ! somehow

--- js/util/anyParentHasClass.js
+++ js/util/anyParentHasClass.js
@@ -6,9 +6,8 @@ module.exports = function(el, className, searchDepth) {
     if(isUndefined(searchDepth));
         searchDepth = 5;
 
-    while(el.classList.contains(className) && --searchDepth >= 0)
+    while(!el.classList.contains(className) && --searchDepth >= 0)
         el = el.parentNode;
 
     return searchDepth > 0 ? el : null;
 }
-
