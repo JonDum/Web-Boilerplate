d9465fc7b7b112067f23dcec1508ac59eab497ab
Jonathan Dumaine <jonathan.dumaine@dumstruck.com>
2017-08-14 17:29:12 -0700

    Rehab webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,54 @@
-
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
 
-    entry: ['js/main'],
+	entry: ['js/main'],
 
-    output: {
-        path: path.join(__dirname, '/build/js'),
-        publicPath: 'js/',
-        filename: 'main.js',
-        chunkFilename: 'chunks/[name].[chunkhash].js',
-        pathinfo: true,
-    },
+	output: {
+		path: path.join(__dirname, '/build/js'),
+		publicPath: 'js/',
+		filename: 'main.js',
+		chunkFilename: 'chunks/[name].[chunkhash].js',
+		pathinfo: true,
+	},
 
-    resolve: {
-        modules: [path.join(__dirname, '/src'), 'node_modules', 'css', 'js', 'js/lib', 'templates'],
-        extensions: ['.js', '.styl', '.html'],
-    },
+	resolve: {
+		modules: [path.join(__dirname, '/src'), 'node_modules', 'css', 'js', 'js/lib', 'templates'],
+		extensions: ['.js', '.styl', '.html'],
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|lib)/,
-                use: [
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|lib)/,
+				use: [
 					{
 						loader: 'babel-loader',
-						options: { presets: [ ['es2015', { modules: false }]] }
+						options: {
+							presets: [['es2015', {
+								modules: false
+							}]]
+						}
 					}
 				]
             },
-            {
+			{
 				test: /\.styl$/,
 				use: [
-					{loader: 'style-loader'},
-					{loader: 'css-loader'},
-					{loader: 'stylus-loader', options: { use: [(require('nib')())], import: [__dirname + '/css/includes/*']}}
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'stylus-loader', options: { use: [(require('nib')())], import: [__dirname + '/css/includes/*'] } }
 				],
 			},
-            {test: /\.css$/,  use: ['style-loader','css-loader']},
-            {test: /\.png$/,  use: ['url-loader?limit=100000&mimetype=image/png']},
-            {test: /\.jpg$/,  use: ['file-loader']},
-            {test: /\.html/,  use: ['ractive-loader']},
-            {test: /\.txt/,   use: ['raw-loader']},
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+			{ test: /\.png$/, use: ['url-loader?limit=100000&mimetype=image/png'] },
+			{ test: /\.jpg$/, use: ['file-loader'] },
+			{ test: /\.html/, use: ['ractive-loader'] },
+			{ test: /\.txt/, use: ['raw-loader'] },
         ]
-    },
+	},
 
 	plugins: [
 		new webpack.DefinePlugin({
@@ -54,12 +57,12 @@ module.exports = {
 		})
     ],
 
-    devtool: "cheap-source-map",
+	devtool: "cheap-source-map",
 
-    devServer: {
-        contentBase: './build',
-        colors: true,
-        inline: true,
-    },
+	devServer: {
+		contentBase: './build',
+		colors: true,
+		inline: true,
+	},
 
 }
