eeab208c3862d23bb8026fa5c72e916ba5466516
Jonathan Dumaine <jonathan.dumaine@dumstruck.com>
2017-08-17 13:38:29 -0700

    Add babel-add-module-exports

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,12 @@ module.exports = {
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: [['es2015', {
-								modules: false
-							}]]
+							presets: [
+								['es2015', { modules: false }],
+							],
+							plugins: [
+								"add-module-exports"
+							]
 						}
 					}
 				]
